778d852c8322f894ad61fc9521c05ab7e998d351
Dave <davidwatson2323@gmail.com>
2018-05-09 23:34:11 +0100

    tests now use promises instead of callbacks

--- spec/api-spec.js
+++ spec/api-spec.js
@@ -1,21 +1,22 @@
 process.env.NODE_ENV = 'test';
 
 const request = require('supertest');
-const {
-  expect
-} = require('chai');
+const {expect} = require('chai');
 const saveTestData = require('../seed/test.seed.js');
 const mongoose = require('mongoose');
 const app = require('../server').app;
 
+mongoose.promise = Promise;
+
 describe('API', () => {
   let usefulData = {};
-  beforeEach(() => {
+  beforeEach(()=> {
     return mongoose.connection
       .dropDatabase()
       .then(saveTestData)
       .then(data => {
         usefulData = data;
+        
       })
       .catch();
   });
@@ -26,132 +27,139 @@ describe('API', () => {
 
 
 
-  describe(' /API  routes', function () {
+
+  describe('Routes', function () {
 
     describe('GET /users', function () {
 
-      it('/users responds with users array', function (done) {
-        request(app)
+      it('/users responds with users array',  ()=> {
+        return request(app)
           .get('/api/users')
-          .end((err, res) => {
+          .expect(200)
+          .then((res) => {
             expect(res.body.length).to.equal(1);
-            expect(res.status).to.equal(200);
-            done();
+          })
+          .catch(err =>{
+            throw err;
           });
       });
 
-      it(' /users/:username responds with user with specified username', function (done) {
+      it(' /users/:username responds with user with specified username',()=>{
         let userID = usefulData.user.username;
-        request(app)
+        return request(app)
           .get(`/api/users/${userID}`)
-          .end((err, res) => {
+          .expect(200)
+          .then((res) => {
             expect(res.body[0].username).to.equal(userID);
-            expect(res.status).to.equal(200);
-            done();
+          })
+          .catch(err =>{
+            throw err;
           });
 
       });
 
-      it(' /users/:username responds with 404 if specified username does not exist', function (done) {
+      it(' /users/:username responds with 404 if specified username does not exist', ()=>{
         let userID = 'dave';
-        request(app)
+        return request(app)
           .get(`/api/users/${userID}`)
-          .end((err, res) => {
-            expect(res.status).to.equal(404);
-            done();
+          .expect(404)
+          .catch(err =>{
+            throw err;
           });
-
       });
 
-
-      it('/users/:username/comments responds with specified users comments', function (done) {
+      it('/users/:username/comments responds with specified users comments', ()=>{
         let userID = usefulData.user.username;
 
-        request(app)
+        return request(app)
           .get(`/api/users/${userID}/comments`)
-          .end((err, res) => {
+          .expect(200)
+          .then((res) => {
             expect(res.body[0].created_by).to.equal(userID);
-            expect(res.status).to.equal(200);
-            done();
+          })
+          .catch(err =>{
+            throw err;
           });
       });
+      it('/users/:username/articles responds with specified users articles', ()=>{
+        let userID = usefulData.user.username;
 
-      it('/users/:username/comments responds with 404 if specified username does not have any comments', function (done) {
-        let userID = 'dave';
-
-        request(app)
-          .get(`/api/users/${userID}`)
-          .end((err, res) => {
-            expect(res.status).to.equal(404);
-            done();
+        return request(app)
+          .get(`/api/users/${userID}/articles`)
+          .expect(200)
+          .then((res) => {
+            expect(res.body[0].created_by).to.equal(userID);
+          })
+          .catch(err =>{
+            throw err;
           });
-
       });
 
     });
 
     describe('GET /topics', function () {
 
-      it('/topics should respond with an array of topics', function (done) {
+      it('/topics should respond with an array of topics', ()=>{
 
-        request(app)
+        return request(app)
           .get('/api/topics')
-          .end((err, res) => {
-            expect(res.body.length).to.equal(3);
-            expect(res.status).to.equal(200);
-            done();
+          .expect(200)
+          .then((res) => {
+            expect(res.body.length).to.equal(3); 
+          })
+          .catch(err =>{
+            throw err;
           });
-
-
       });
 
-      it('topics/:topicName/articles should respond with an array of articles on a topic', function (done) {
-        let topic = 'football';
-        request(app)
+      it('topics/:topicName/articles should respond with an array of articles on a topic', ()=>{
+        let topic = usefulData.articles[0].belongs_to;
+        return request(app)
           .get(`/api/topics/${topic}/articles`)
-          .end((err, res) => {
-            expect(res.body.length).to.equal(1);
-            expect(res.status).to.equal(200);
-            done();
+          .expect(200)
+          .then(res => {
+            expect(res.status).to.equal(200);           
+          })
+          .catch(err =>{
+            throw err;
           });
       });
 
-      it('topics/:topicName/articles respond with an 404 if topic has no articles', function (done) {
+      it('topics/:topicName/articles respond with an 404 if topic has no articles',  ()=>{
         let topic = 'tennis';
-        request(app)
+        return request(app)
           .get(`/api/topics/${topic}/articles`)
-          .end((err, res) => {
-
-            expect(res.status).to.equal(404);
-            done();
+          .expect(404)
+          .catch(err =>{
+            throw err;
           });
-
-
       });
     });
 
     describe(' PUT /comments/:id', function () {
 
-      it('should return an updated articles with increased vote count', function (done) {
+      it('should return an updated articles with increased vote count', ()=>{
         let commentId = usefulData.comments[0]._id;
-        request(app)
+        return request(app)
           .put(`/api/comments/${commentId}?vote=up`)
-          .end((err, res) => {
-
-            expect(res.body.votes).to.equal(1);
-            expect(res.status).to.equal(200);
-            done();
+          .expect(200)
+          .then(res => {
+            expect(res.body.votes).to.equal(1);          
+          })
+          .catch(err =>{
+            throw err;
           });
       });
 
-      it('should return a 404 if voted comment does not exist', function (done) {
+      it('should return a 404 if voted comment does not exist', ()=>{
         let commentId = '1234';
-        request(app)
+        return request(app)
           .put(`/api/comments/${commentId}?vote=up`)
-          .end((err, res) => {
-            expect(res.status).to.equal(404);
-            done();
+          .expect(404)
+          .catch(err =>{
+            throw err;
           });
+    
       });
 
 
@@ -159,92 +167,95 @@ describe('API', () => {
 
     describe(' GET /articles', function () {
 
-      it('should respond with array of articles', function (done) {
-        request(app)
+      it('should respond with array of articles', () =>{
+        return request(app)
           .get('/api/articles')
-          .end((err, res) => {
-
+          .expect(200)
+          .then((res) => {
             expect(res.body.length).to.equal(2);
-            expect(res.status).to.equal(200);
-            done();
-          });
+          })
+          .catch(err =>{
+            throw err;
+          });         
       });
 
-      it('/articles/:id should respond with artices with specified ID', function (done) {
+      it('/articles/:id should respond with artices with specified ID', ()=>{
 
         let articleId = usefulData.articles[0]._id;
-        request(app)
+        return request(app)
           .get(`/api/articles/${articleId}`)
-          .end((err, res) => {
-
+          .expect(200)
+          .then((res) => {
             expect(res.body.length).to.equal(1);
-            expect(res.body[0].belongs_to).to.equal('cats');
-            expect(res.status).to.equal(200);
-            done();
-          });
+            expect(res.body[0].belongs_to).to.equal( usefulData.articles[0].belongs_to);
+          })
+          .catch(err =>{
+            throw err;
+          });             
       });
 
-      it(' /articles/:id should respond with  404 if artices with specified ID does not exist', function (done) {
+      it(' /articles/:id should respond with  404 if articles with specified ID does not exist', ()=>{
 
         let articleId = '4423ffewwewerwqf';
 
-        request(app)
+        return request(app)
           .get(`/api/articles/${articleId}`)
-          .end((err, res) => {
-            expect(res.status).to.equal(404);
-            done();
+          .expect(404)
+          .catch(err =>{
+            throw err;
           });
       });
 
 
-      it('/articles/:articleID/comments should respond with array of comments of a specified article', function (done) {
+      it('/articles/:articleID/comments should respond with array of comments of a specified article', ()=>{
         let articleId = usefulData.articles[0]._id;
 
-        request(app)
+        return request(app)
           .get(`/api/articles/${articleId}/comments`)
-          .end((err, res) => {
-
-            expect(res.body.length).to.equal(2);
-            expect(res.status).to.equal(200);
-            done();
+          .expect(200)
+          .then((res) => {
+            expect(res.body.length).to.equal(2); 
+          })
+          .catch(err =>{
+            throw err;
           });
       });
 
-      it('/articles/:articleID/comments should respond with 404  array of comments of a specified article is of length 0', function (done) {
+      it('/articles/:articleID/comments should respond with 404  array of comments of a specified article is of length 0', ()=>{
         let articleId = '';
 
-        request(app)
+        return request(app)
           .get(`/api/articles/${articleId}/comments`)
-          .end((err, res) => {
-            expect(res.status).to.equal(404);
-            done();
+          .expect(404)
+          .catch(err =>{
+            throw err;
           });
+        
       });
     });
 
     describe(' PUT /articles/:id', function () {
 
-      it('should increment vote of article with specified ID', function (done) {
+      it('should increment vote of article with specified ID',()=>{
         let articleId = usefulData.articles[0]._id;
-
-        request(app)
+        return request(app)
           .put(`/api/articles/${articleId}?vote=up`)
-          .end((err, res) => {
-
+          .expect(200)
+          .then((res) => { 
             expect(res.body.votes).to.equal(1);
-            expect(res.status).to.equal(200);
-            done();
+          })
+          .catch(err =>{
+            throw err;
           });
       });
 
-      it('should give 404 article with specified ID does not exist', function (done) {
+      it('should give 404 article with specified ID does not exist', ()=>{
         let articleId = '2345';
-
-        request(app)
+        return request(app)
           .put(`/api/articles/${articleId}?vote=up`)
-          .end((err, res) => {
-            expect(res.status).to.equal(404);
-            done();
+          .expect(404)
+          .catch(err =>{
+            throw err;
           });
       });
 
@@ -252,24 +263,18 @@ describe('API', () => {
 
     describe(' POST /articles/:id/comments', function () {
 
-      it('should add new comment to article with specified ID', function (done) {
+      it('should add new comment to article with specified ID',()=>{
         let articleId = usefulData.articles[1]._id;
-
         request(app)
           .post(`/api/articles/${articleId}/comments`)
           .send({
-            comment: 'hello',
-            belongs_to: articleId
+            comment: 'hello'
           })
-          .end((err, res) => {
-
+          .expect(201)
+          .then((res) => {            
             expect(res.body.body).to.equal('hello');
             expect(res.body.belongs_to).to.equal(`${articleId}`);
-            expect(res.status).to.equal(200);
-            done();
           });
-
-
       });
     });
   });
