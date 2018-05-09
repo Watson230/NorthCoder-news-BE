4c509d2c9008a34ba6938c58a43ea12726752eb2
Dave <davidwatson2323@gmail.com>
2018-05-09 23:41:29 +0100

    renamed fetch topics func

--- routes/topicsRoute.js
+++ routes/topicsRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const topicCtrl = require('../controllers/topicsCtrl.js');
 
 
-router.get('/', topicCtrl.fetchTopicsAPI);
+router.get('/', topicCtrl.fetchTopics);
 
 router.get('/:topicName/articles', topicCtrl.fetchTopicsArticles);
 
