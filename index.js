7a07818e4cc130076ae272fdc6ee7206c7f51119
Dave <davidwatson2323@gmail.com>
2018-05-09 23:40:30 +0100

    used PORT var to import process.env.port

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 /*eslint-disable no-console*/
 const app = require('./server').app;
+const PORT = process.env.PORT;
 
 
-app.listen(process.env.PORT, function () {
+app.listen(PORT, function () {
   console.log(`listening on port ${process.env.PORT}`);
 });
 
